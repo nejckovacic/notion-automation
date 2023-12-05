ce41ac1a801be9097450cdba7e4b106ca84a244d
Nejc Kovačič <40825221+nejckovacic@users.noreply.github.com>
2023-12-05 12:04:01 +0100

    Update notion-automation.py

--- notion-automation.py
+++ notion-automation.py
@@ -1,7 +1,11 @@
 import os
 from notion_client import Client
 
-notion = Client(auth=os.environ["NOTION_TOKEN"])
+notion_token = os.environ.get('NOTION_TOKEN')
+if not notion_token:
+    raise ValueError("No NOTION_TOKEN found in environment variables")
+
+notion = Client(auth=notion_token)
 
 new_page_data = {
     "parent": {"database_id": "a2e30c8fddfe4c78af393e767afcc4af"},
