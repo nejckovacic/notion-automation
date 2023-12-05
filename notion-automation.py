aea50e872d6bd312cd5b44746f503bd14874b0cb
Nejc Kovačič <40825221+nejckovacic@users.noreply.github.com>
2023-12-05 11:57:47 +0100

    Update notion-automation.py

--- notion-automation.py
+++ notion-automation.py
@@ -1,6 +1,7 @@
+import os
 from notion_client import Client
 
-notion = Client(auth="your-integration-token")
+notion = Client(auth=os.environ["NOTION_TOKEN"])
 
 new_page_data = {
     "parent": {"database_id": "a2e30c8fddfe4c78af393e767afcc4af"},
