9450f38496514a5cd3b3b77db387b4cba59fdf99
nejcKovacic <nejc.kovacic@kalmia.si>
2023-12-10 16:20:17 +0100

    getting started

--- python/main.py
+++ python/main.py
@@ -4,13 +4,22 @@ import os
 from notion_api import NotionAPI
 from utils import format_page_data
 
-NOTION_TOKEN = os.environ.get("NOTION_TOKEN")
+# Try to get NOTION_TOKEN from OS environment variables
+NOTION_TOKEN = os.getenv("NOTION_TOKEN")
+
+# If NOTION_TOKEN is not found, then load from .env file
 if NOTION_TOKEN is None:
-    raise ValueError("No NOTION_TOKEN found in environment variables")
+    from dotenv import load_dotenv
+    load_dotenv()
+    NOTION_TOKEN = os.getenv("NOTION_TOKEN")
+
+# Raise error if NOTION_TOKEN is still None after loading .env
+if NOTION_TOKEN is None:
+    raise ValueError("No NOTION_TOKEN found in environment variables or .env file")
 
 notion = NotionAPI(NOTION_TOKEN)
 
-
+# Rest of your script
 database_id = "a2e30c8fddfe4c78af393e767afcc4af"
 new_page_title = "New Test Page 222"
 additional_properties = {}  # Add additional properties if needed
